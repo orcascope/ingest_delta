3af40e1bcf731cc3e73c3cfff65e94c39cd063d2
Arshadh <30562546+orcascope@users.noreply.github.com>
2023-10-03 11:57:44 -0400

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,3 +1,9 @@
+-- Creates the target and staging table. 
+-- Perform Day 1 load into stage
+-- Merge into target on non matching hash and subsequent match on PK
+-- Perform Day 2 load into stage
+-- Merge into target as above.
+  
 CREATE OR REPLACE SCHEMA LANDING;
 
 USE SCHEMA LANDING;
@@ -24,3 +30,17 @@ ON t.id = s.id
 WHEN MATCHED THEN UPDATE SET t.id=s.id, t.email=s.email, t.row_hash=s.row_hash
 WHEN NOT MATCHED THEN INSERT (id, email, row_hash) VALUES (s.id. s.email, s.row_hash);
 
+--This should result in the 3 records inserted into target.
+
+--Day 2 Records
+Truncate table stage_customer;
+insert into stage_customer values (2, 'bob@aol.com', 52), (3, 'c@gmail.com', 43), (4, 'dan@danfoss.com', 54) ;
+
+-- Same merge code as above, repeated for clarity.
+MERGE into customer t 
+USING (Select * from stage_customer s where not exists (select * from customer t where t.row_hash=s.row_hash)) s
+ON t.id = s.id 
+WHEN MATCHED THEN UPDATE SET t.id=s.id, t.email=s.email, t.row_hash=s.row_hash
+WHEN NOT MATCHED THEN INSERT (id, email, row_hash) VALUES (s.id. s.email, s.row_hash);
+
+--This should result in the 1 record (id=4) inserted into target and 1 record(id=2) updated with latest values.
